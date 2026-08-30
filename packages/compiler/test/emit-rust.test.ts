1fb99c22ca94ec2fc8d2910278f82359c68557c7
filipeforattini <filipeforattini1@gmail.com>
2026-08-30 15:18:04 -0300

    test: cover concurrent settled mapping in Rust

--- packages/compiler/test/emit-rust.test.ts
+++ packages/compiler/test/emit-rust.test.ts
@@ -969,6 +969,7 @@ shardedTest.each(shardSelect([
   "1445-exit-process-exit.ts",
   "1446-exit-uncaught.ts",
   "1448-stream-columns.ts",
+  "1453-pmap-settled.ts",
   "1454-nonnull-narrow-and-fcc-bytes.ts",
   "518-promise-void-union-callbacks.ts",
   "1429-promise-catch-finally.ts",
